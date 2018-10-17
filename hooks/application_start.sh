3d09ff3cdc36ebd76924ff28adf6f0eb85190b69
katainaka0503 <katainaka0503@gmail.com>
2018-10-17 11:32:52 +0900

    before install

--- hooks/application_start.sh
+++ hooks/application_start.sh
@@ -5,8 +5,6 @@ forever_log="/var/log/app/forever.log"
 app_access_log="/var/log/app/access.log"
 app_error_log="/var/log/app/error.log"
 
-touch $forever_log
-
 forever start -a \
   -l "$forever_log" \
   -o "$app_access_log" \
