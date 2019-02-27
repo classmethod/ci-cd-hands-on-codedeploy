3826d24d33ceeb74dbb06457f1abd2449768be71
Aritomo <kari_2580@yahoo.co.jp>
2019-02-27 20:07:37 +0900

    :fire: あえてテスト落とすように修正

--- src/model/fizzbuzz.js
+++ src/model/fizzbuzz.js
@@ -6,7 +6,7 @@ module.exports = (number) => {
       result.push("FizzBuzz");
     } else if (i % 3 == 0) {
       result.push("Fizz");
-    } else if (i % 5 == 0) {
+    } else if (i % 10 == 0) {
       result.push("Buzz");
     } else {
       result.push(i);
